98f4d579aecdbfdc7e32ec57b02c7b7a50f29bd5
PraneshASP <praneshas2000@gmail.com>
2022-10-12 15:56:03 +0800

    ♻️ Add 'asmOptimizer' for active file

--- src/extension.js
+++ src/extension.js
@@ -99,6 +99,42 @@ function onActivate(context) {
       }
     )
   );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      EXTENSION_PREFIX + ".activeFile.asmOptimizer",
+      async (args) => {
+        let activeDoc = vscode.window.activeTextEditor.document;
+        let activeFile = activeDoc.fileName;
+
+        if (activeFile.endsWith(".sol")) {
+          const contractPathArray = activeFile.split("/"); //activeFile.substring(0, activeFile.length - 4);
+          let contractName = contractPathArray[contractPathArray.length - 1];
+          contractName = contractName.substring(0, contractName.length - 4);
+          contractPathArray.pop();
+
+          let contractDir = await getAppRootDir(contractPathArray.join("/"));
+
+          cp.exec(
+            `cd ${contractDir} && forge inspect ${contractName} asm-optimized`,
+            (err, stdout, stderr) => {
+              if (err) {
+                vscode.window.showErrorMessage(
+                  `[Optimization failed] ${contractName}\n${err.message}\n\nNOTE: Please make sure to install 'forge' before using this extension`
+                );
+                console.error(err);
+              }
+              newWindowBeside(stdout);
+            }
+          );
+        } else {
+          vscode.window.showErrorMessage(
+            `[Optimization failed] ${activeFile}\n\nOnly .sol files in a Foundry project are supported for now.`
+          );
+        }
+      }
+    )
+  );
 }
 
 /* exports */
