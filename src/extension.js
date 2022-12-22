9eeca7d1a077ff6da7bbc196874fe6300bc959be
PraneshASP <praneshas2000@gmail.com>
2022-12-23 00:16:32 +0530

    🚚 refactor ir-optimzer active file command

--- src/extension.js
+++ src/extension.js
@@ -7,73 +7,22 @@
 /** imports */
 const vscode = require("vscode");
 const cp = require("child_process");
-const path = require("path");
-const fs = require("fs");
+const { irOptimizerActiveFile } = require("./commands/ir-optimizer-active");
+const { newWindowBeside, getContractRootDir } = require("./helpers");
 
 /** global vars */
 const LANGID = "solidity";
 const EXTENSION_PREFIX = "vscode-solidity-inspector";
 
-function newWindowBeside(content) {
-  vscode.workspace
-    .openTextDocument({ content: content, language: LANGID })
-    .then((doc) =>
-      vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside)
-    );
-}
-
-function getContractRootDir(currentDir) {
-  while (
-    !fs.existsSync(path.join(currentDir, "foundry.toml")) &&
-    !fs.existsSync(path.join(currentDir, "hardhat.config.js")) &&
-    !fs.existsSync(path.join(currentDir, "hardhat.config.ts"))
-  ) {
-    currentDir = path.join(currentDir, "..");
-  }
-
-  console.log("**Current dir", currentDir);
-  return currentDir;
-}
+/** register commands  */
+const irOptimizerActiveFileSubscription = vscode.commands.registerCommand(
+  EXTENSION_PREFIX + ".activeFile.irOptimizer",
+  irOptimizerActiveFile
+);
 
 /** event funcs */
 function onActivate(context) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand(
-      EXTENSION_PREFIX + ".activeFile.irOptimizer",
-      async (args) => {
-        let activeDoc = vscode.window.activeTextEditor.document;
-        let activeFile = activeDoc.fileName;
-
-        if (activeFile.endsWith(".sol")) {
-          const contractPathArray = activeFile.split("/"); //activeFile.substring(0, activeFile.length - 4);
-          let contractName = contractPathArray[contractPathArray.length - 1];
-          contractName = contractName.substring(0, contractName.length - 4);
-          contractPathArray.pop();
-
-          let contractDir = await getContractRootDir(
-            contractPathArray.join("/")
-          );
-
-          cp.exec(
-            `cd ${contractDir} && forge inspect ${contractName} ir-optimized`,
-            (err, stdout, stderr) => {
-              if (err) {
-                vscode.window.showErrorMessage(
-                  `[Optimization failed] ${contractName}\n${err.message}\n\nNOTE: Please make sure to install 'forge' before using this extension`
-                );
-                console.error(err);
-              }
-              newWindowBeside(stdout);
-            }
-          );
-        } else {
-          vscode.window.showErrorMessage(
-            `[Optimization failed] ${activeFile}\n\nOnly .sol files in a Foundry project are supported for now.`
-          );
-        }
-      }
-    )
-  );
+  context.subscriptions.push(irOptimizerActiveFileSubscription);
 
   context.subscriptions.push(
     vscode.commands.registerCommand(
