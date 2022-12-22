bd8d2d21a9061d43ad056b214e38cd48884f12b6
PraneshASP <praneshas2000@gmail.com>
2022-12-23 00:14:36 +0530

    🚚 refactor ir-optimzer active file command

---
+++ src/helpers.js
@@ -0,0 +1,25 @@
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
+
+function getContractRootDir(currentDir) {
+  while (
+    !fs.existsSync(path.join(currentDir, "foundry.toml")) &&
+    !fs.existsSync(path.join(currentDir, "hardhat.config.js")) &&
+    !fs.existsSync(path.join(currentDir, "hardhat.config.ts"))
+  ) {
+    currentDir = path.join(currentDir, "..");
+  }
+
+  return currentDir;
+}
+
+function newWindowBeside(content) {
+  vscode.workspace
+    .openTextDocument({ content: content, language: LANGID })
+    .then((doc) =>
+      vscode.window.showTextDocument(doc, vscode.ViewColumn.Beside)
+    );
+}
+
+module.exports = { newWindowBeside, getContractRootDir, LANGID };
